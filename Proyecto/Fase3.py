352d6f0436c51f9b830117f1572f122a5a479910
Kalid Ruiz <aalanruiz889@gmail.com>
2024-11-29 10:00:38 -0600

    Inicio de Fase 3

--- Proyecto/Fase3.py
+++ Proyecto/Fase3.py
@@ -1,4 +1,18 @@
 import re 
 
-# Elaboración de la Fase 1 de GitHub 
+# Elaboración de la Fase 3 del proyecto de ensambladores 
+
+class Decodificacion:
+
+    def __init__(self):
+        pass
+
+    '''
+    Decodificación de instrucciones 
+    dependiendo de la cantidad de operandos que estos tienen     
+    '''
+    def decodificar_sin_operandos(self,instruccion):
+        pass
+
+
 
