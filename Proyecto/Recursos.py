4df15229098b298b03ca74818e363f4d5a8f851c
Kalid Ruiz <aalanruiz889@gmail.com>
2024-11-29 10:08:48 -0600

    Agregación decodificaciones 0 operandos

--- Proyecto/Recursos.py
+++ Proyecto/Recursos.py
@@ -4,6 +4,7 @@
 pseudoinstrucciones = {"dw","db","equ","end","ends","endp",".code",".stack",".data"}
 
 segmentos = {".stack segment", ".data segment", ".code segment"}
+
 # Registros 
 registros_16 =  {"ax","bx","cx","dx"}
 
@@ -17,3 +18,21 @@ instrucciones_0 = {"aam", "aas", "iret", "lahf", "std", "sti"}
 instrucciones_1 = {"int", "not", "idiv", "mul", "jo", "loop"}
 instrucciones_2 = {"rcl","shl","xchg", "mov", "jna", "jnc", "jnl", "jbe"}
 
+# Decodificacion de instrucciones segun el número de operandos 
+
+decodificaciones_0 = {
+    'aam': '1101010000001010',
+    'aas': '00111111',
+    'iret':'11001111',
+    'std': '11111101',
+    'sti': '11111011',
+    'lahf':'10011111'
+}
+
+decodificaciones_1 = {
+
+}
+
+decodificaciones_2 = {
+    
+}
