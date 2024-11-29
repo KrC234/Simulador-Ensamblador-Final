ac7dd59b521cfcdd523a70bd4c47cb9fdeb66da5
Kalid Ruiz <aalanruiz889@gmail.com>
2024-11-29 12:19:47 -0600

    Actualización diccionario y agregación en fase 3

--- Proyecto/Fase3.py
+++ Proyecto/Fase3.py
@@ -17,8 +17,35 @@ class Decodificacion:
             return 'Instruccion no reconocida'
         
         binario = r.decodificaciones_0[instruccion]
-        hexadecimal = hex(int(binario,2))
+        codificacion = hex(int(binario,2))
 
-        return hexadecimal
+        return codificacion
+    
+    def decodificar_un_operando(self, instruccion, operando):
+        if instruccion not in r.instrucciones_1:
+            return 'Instrucción no reconocida'
+    
+        tipo = None 
+        w = None 
+
+        if operando in r.registros_8:
+            tipo = 'reg/mem'
+            w = '0'
+        elif operando in r.registros_16:
+            tipo = 'reg/mem'
+            w = '1'
+        
+        instruccion_data = r.decodificaciones_1[instruccion]
+        if instruccion_data['operando'] != tipo:
+            return 'Operando no valido'
+        
+        opcode = instruccion_data['opcode'].replace('w',w)
+        mod = '11'
+        reg = instruccion_data['reg']
+        r_m = r.registros_binarios[operando]
+
+        binario = f'{opcode}{mod}{reg}{r_m}'
+        codificacion = hex(int(binario,2))
+        return codificacion
 
 
--- Proyecto/Recursos.py
+++ Proyecto/Recursos.py
@@ -1,5 +1,7 @@
-# Definicion de las palabras reservadas para el análisis del codigo 
-
+'''
+Definición de palabras reservadas 
+Definición de diccionario de datos 
+'''
 # Pseudoinstrucciones 
 pseudoinstrucciones = {"dw","db","equ","end","ends","endp",".code",".stack",".data"}
 
@@ -18,6 +20,19 @@ instrucciones_0 = {"aam", "aas", "iret", "lahf", "std", "sti"}
 instrucciones_1 = {"int", "not", "idiv", "mul", "jo", "loop"}
 instrucciones_2 = {"rcl","shl","xchg", "mov", "jna", "jnc", "jnl", "jbe"}
 
+# Valores binarios de los registros para operandos reg o r/m 
+
+registros_binarios = {
+    'ax': '000', 'al': '000',
+    'cx': '001', 'cl': '001',
+    'dx': '010', 'dl': '010',
+    'bx': '011', 'bl': '011',
+    'sp': '100', 'ah': '100',
+    'bp': '101', 'ch': '101',
+    'si': '110', 'dh': '110',
+    'di': '111', 'bh': '111'
+}
+
 # Decodificacion de instrucciones segun el número de operandos 
 
 decodificaciones_0 = {
@@ -29,10 +44,35 @@ decodificaciones_0 = {
     'lahf':'10011111'
 }
 
+# La mayoría de instrucciones cuentan con una dirección de mod reg r/m
 decodificaciones_1 = {
-
+    'int':{
+        'operando': 'inmediato',
+        'opcode' : '1100101',
+    },
+    'not':{
+        'operando': 'reg/mem',
+        'opcode' : '1111011w',
+        'mod' : None, 
+        'reg' : '010',
+        'r/m' : None
+    },
+    'idiv':{
+        'operando': 'reg/mem',
+        'opcode' : '1111011w',
+        'mod' : None,
+        'reg' : '111',
+        'r/m' : None
+    },
+    'mul':{
+        'operando': 'reg/mem',
+        'opcode' : '1111011w',
+        'mod' : None,
+        'reg' : '100',
+        'r/m' : None
+    }
 }
 
 decodificaciones_2 = {
-    
+
 }
