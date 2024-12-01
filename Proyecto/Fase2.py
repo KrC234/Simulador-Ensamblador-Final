201ab1a7ff07fdaf009f6195a2e76913b93aa7b7
Kalid Ruiz <aalanruiz889@gmail.com>
2024-12-01 01:19:48 -0600

    Agregación y Corrección Fase 2

--- Proyecto/Fase2.py
+++ Proyecto/Fase2.py
@@ -5,7 +5,8 @@ class Validacion:
     def __init__(self):
         self.pc = hex(0)
         self.lista_de_simbolos = []
-
+        self.variables = []
+        self.etiquetas = []
     # !Voy a omitir de momento el tratamiento de segmentos para tratarlos de mejor manera
 
 
@@ -19,11 +20,33 @@ class Validacion:
     
     def verificar_en_data(self,linea):
         for exp in r.data_patterns:
-            if re.match(exp,linea.strip()):
+            match = re.match(exp,linea.strip())
+            if match:
+                nombre = linea.split()[0]
+                valor = match.group(2)
+                tamaño =  len(valor)
+                self.registrar_simbolo(nombre,tipo='Variable',valor=valor,direccion=self.pc,codificacion='NA',tamaño = tamaño)
                 return True
         return False 
     
+    #* Registro de variables y etiquetas
+    def registrar_variable(self,variable,valor):
+        pass
 
+    def registrar_etiqueta(self,etiqueta):
+        self.etiquetas.append(etiqueta)
+
+    def registrar_simbolo(self, simbolo, tipo, valor =  None,direccion = None, codificacion = None, tamaño = None):
+        simbolo = {
+            'nombre' : simbolo, 
+            'tipo' : tipo,
+            'valor': valor,
+            'codificacion': codificacion,
+            'tamaño': tamaño,
+            'direccion': direccion,
+        }
+        self.lista_de_simbolos.append(simbolo)
+        
     '''
     Tratamiento del code segment 
     Se requiere validar los registros
@@ -31,4 +54,113 @@ class Validacion:
     Etiquetas
     '''
 
+    def validar_instruccion(self,instruccion,instrucciones):
+        if instruccion in instrucciones:
+            return True
+        else:
+            return False 
+        
+    def validar_registro(self,operando):
+        if operando in r.registros_8:
+            return True
+        if operando in r.registros_16:
+            return True
+        if operando in r.registros_base:
+            return True
+        return False 
+        
+    def validar_operandos(self,operando1,operando2):
+
+        if operando1 in r.registros_8 and operando2 in r.registros_8:
+            return True
+        elif operando1 in r.registros_16 and operando2 in r.registros_16:
+            return True
+        elif operando1 in r.registros_base and operando2 in r.registros_base:
+            return True
+        
+        if (re.match(r"^\d+$", operando2) or re.match(r"^[0-9A-Fa-f]+h$", operando2)) and (
+                operando1 in self.registros_16 or operando1 in self.registros_8):
+            return True
+        
+    def validar_operando(self,operando):
+        if self.validar_registro(operando):
+            return True
+        elif operando in self.etiquetas:
+            return True
+        return False 
+
+    def validar_linea(self,linea):
+        if ':' in linea:
+            etiqueta = linea.split(':')[0].strip
+            self.registrar_etiqueta(etiqueta)
+
+        # instrucciones de dos operandos 
+        match_dos = re.match(r"^\s*(\w+)\s+(\w+),\s*(\w+)$", linea.strip()) 
+        if match_dos:
+            instruccion, operandoDestino, operandoFuente = match_dos.groups()
+            if not self.validar_instruccion(instruccion, r.instrucciones_2):
+                return f'Error: {instruccion} no es una instruccion de dos operandos'
+            
+            if not self.validar_operandos(operandoDestino,operandoFuente):
+                return f'Error: {operandoDestino} y {operandoFuente} no son compatibles o validos'
+            
+            return 'Correcta'
+        
+        # instrucciones de un operando 
+        match_uno = re.match(r"^\s*(\w+)\s+(\w+)$", linea.strip())
+        if match_uno:
+            instruccion, operando =  match_uno.groups()
+            if not self.validar_instruccion(instruccion,r.instrucciones_1):
+                return f'{instruccion} no es de un operando'
+            if not self.validar_operando(operando):
+                return f'{operando} no es valido'
+            return 'Correcta'
+        
+        # instrucciones sin operando 
+        if re.match(r"^\s*(\w+)\s*$", linea.strip()):
+            instruccion = linea.strip()
+            if self.validar_instruccion(instruccion,r.instrucciones_0):
+                return 'Correcta'
+            else: 
+                return f'Error: {instruccion} no es una instruccion valida'
+            
+        return 'Error de declaracion'
+
+    def analizar_linea(self,linea,segmento_activo):
+        if not segmento_activo:
+            return 'Error: Linea fuera de segmento'
+        if self.verificar_en_stack(linea):
+            return 'Correcta'
+        elif self.verificar_en_data(linea):
+            return 'Correcta'
+        elif segmento_activo:
+            return self.validar_instruccion(linea)
+        else:
+            return 'Declaracion incorrecta o desconocida'
+
+    def analizar_lineas(self,lineas):
+        resultados = []
+
+        segmento_activo = False
+
+        for linea in lineas:
+
+            if linea in r.segmentos:
+                segmento_activo = True
+                resultados.append(self.retornar_segmento(linea))
+            elif linea == 'ends':
+                segmento_activo = False 
+            else: 
+                resultado = self.analizar_linea(linea,segmento_activo)
+        resultados.append(resultado)
+
     
+
+
+    def retornar_segmento(self,linea):
+        if linea == '.stack segment':
+            return f'{linea} - Segmento de Pila'
+        elif linea == '.data segment':
+            return f'{linea} - Segmento de Datos'
+        elif linea == '.code segment':
+            return f'{linea} - Segmento de Codigo'
